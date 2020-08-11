5767820c46b2b08ad43f406d3563ea86d1372c8e
zkj <438101845@qq.com>
2020-08-11 20:46:23 +0800

    [zkj]-{feat}: add AboutMe.css

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './AboutMe.css';
 
 class AboutMe extends React.Component {
   render() {
     return (
       <div className="aboutMe">
-        <h2 className="aboutMe title">ABOUT ME</h2>
-        <p>
+        <h2 className="aboutMe-title">ABOUT ME</h2>
+        <p className="aboutMe-content">
           Lorem ipsum dolor sit amet,consectetur adoposicing elit. Repellendus, 
           non, dolorem, cumque distinctio magni quan wxpedlita velit laborum 
           sunt amet facere tempora ut fuga aliqyan ad adperiores voliptatem 
